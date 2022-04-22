01fa11706740da5207a06bf78c620bde816e9703
tato <tkats18@freeuni.edu.ge>
2022-04-22 22:26:13 +0400

    scss initialization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import {AppRoutes, AuthorizationRoutes} from "./routes";
+import AuthRoute from "./routes/middleware/AuthRoute";
+import NonAuthRoute from "./routes/middleware/NonAuthRoute";
 
 function App() {
-  return (
-    <div className="App">
-      empty page
-    </div>
-  );
+    const generateAuthRouteComponent = (route, index) => {
+        return <Route
+            key={index}
+            path={route.path}
+            element={<AuthRoute
+                path={route.path}
+                children={route.component}
+                key={index}
+            />}
+        />
+    }
+    const generateNonAuthRouteComponent = (route, index) => {
+        return (
+            <Route
+                key={index}
+                path={route.path}
+                element={<NonAuthRoute
+                    path={route.path}
+                    children={route.component}
+                    key={index}
+                />}
+            />)
+    }
+    return (
+        <Routes>
+            {AppRoutes.map((route, index) => {
+                return generateAuthRouteComponent(route, index)
+            })}
+            {AuthorizationRoutes.map((route, index) => {
+                return generateNonAuthRouteComponent(route, index)
+            })}
+        </Routes>
+    );
 }
 
 export default App;
