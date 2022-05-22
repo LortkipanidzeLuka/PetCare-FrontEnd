75b34c767610d32b7fd328300ca9e57a28d69ec3
tato <tkats18@freeuni.edu.ge>
2022-05-22 20:51:06 +0400

    login refactor to new form

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,11 +1,11 @@
-import { Button, Col, Modal, ModalBody, ModalHeader, Row } from 'reactstrap';
+import { Modal, ModalBody, ModalHeader } from 'reactstrap';
 import { useForm } from 'react-hook-form';
-import Text from '../text/Text';
-import { TextType } from '../text/TextType';
 import Api from 'src/services/index';
 import { useDispatch } from 'react-redux';
 import useToast, { ToastType } from '../../hooks/UseToast';
 import { loginUser } from '../../storage/Actions';
+import { TextFormInput, TextInputType } from '../form/TextFormInput';
+import FormInput from '../form/FormInput';
 
 const AuthModal = ({ open, closeModal }) => {
 	const { register: auth, handleSubmit, formState: { errors } } = useForm({ shouldUseNativeValidation: true });
@@ -25,46 +25,49 @@ const AuthModal = ({ open, closeModal }) => {
 			}
 		}
 	};
+	const DefaultFormConfig = {
+		lg: '12',
+		xl: '12',
+		sm: '12',
+		xs: '12',
+		errors: errors,
+		register: auth
+	};
+	const FormConfig = [
+		{
+			double: false,
+			first: {
+				...DefaultFormConfig,
+				name: 'username',
+				heading: 'Username',
+				placeholder: 'Please enter your e-mail.',
+				requiredMessage: 'E-mail is required.',
+				component: TextFormInput
+			}, second: {}
+		},
+		{
+			double: false,
+			first: {
+				...DefaultFormConfig,
+				name: 'password',
+				heading: 'Password',
+				placeholder: 'Please enter password',
+				requiredMessage: 'Password required.',
+				component: TextFormInput,
+				type:TextInputType.PASSWORD
+			}
+		},
+	];
 
 	return (
 		<Modal isOpen={open} toggle={closeModal} className={'small-modal'}>
 			<ModalHeader>
-				Log in
+				Log In
 			</ModalHeader>
 			<ModalBody>
 				<div>
 					<form onSubmit={handleSubmit(onSubmit)}>
-						<Row className={'mrb-medium'}>
-							<Col xl='4' lg='4' sm='4' xs='12'>
-								<Text text={'Username'} type={TextType.MEDIUM} />
-							</Col>
-							<Col xl='8' lg='8' sm='8' xs='12'>
-								<input
-									className={'form-control'} {...auth('username', { required: 'Please enter username.' })}
-									name={'username'}
-									placeholder={'Enter your username'} type={'text'} required={false} />
-								{errors.username &&
-									<Text text={errors.username.message} type={TextType.SMALL} classNames={['error-text']} />}
-							</Col>
-						</Row>
-						<Row className={'mrb-medium'}>
-							<Col xl='4' lg='4' sm='4' xs='12'>
-								<Text text={'Password'} type={TextType.MEDIUM} />
-							</Col>
-							<Col xl='8' lg='8' sm='8' xs='12'>
-								<input
-									className={'form-control'} {...auth('password', { required: 'Please enter your password.' })}
-									name={'password'}
-									placeholder={'Enter your name'} type={'password'} required={false} />
-								{errors.password &&
-									<Text text={errors.password.message} type={TextType.SMALL} classNames={['error-text']} />}
-							</Col>
-						</Row>
-						<Row>
-							<Col>
-								<Button type='submit'>log in</Button>
-							</Col>
-						</Row>
+						<FormInput FormConfig={FormConfig} buttonName={'Log In'} fullButton/>
 					</form>
 				</div>
 			</ModalBody>
