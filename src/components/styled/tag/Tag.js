1827fb35c81ddcbb80506f6cbe62a3838cd697b3
tato <tkats18@freeuni.edu.ge>
2022-06-01 19:33:16 +0400

    tag improvement

--- src/components/styled/tag/Tag.js
+++ src/components/styled/tag/Tag.js
@@ -2,13 +2,14 @@ import Text from 'src/components/styled/text/Text';
 import { TextType } from 'src/components/styled/text/TextType';
 
 const Tag = (params) => {
-    return (
-        <div style={{backgroundColor: `${params.color} !important` ? params.color : "", ...params.style}} className={"tag"}>
-            <Text text={`#${params.text}`} type={TextType.REGULAR}/>
-            {params.children}
-        </div>
+	return (
+		<div style={{ backgroundColor: `${params.color} !important` ? params.color : '', ...params.style }}
+				 className={`tag ${params.className ? params.className : ''}`}>
+			<Text text={`#${params.text}`} type={TextType.REGULAR} />
+			{params.children}
+		</div>
 
-    )
-}
+	);
+};
 
-export default Tag
+export default Tag;
--- src/components/styled/tag/TagList.js
+++ src/components/styled/tag/TagList.js
@@ -1,18 +1,53 @@
 import Tag from './Tag';
+import { useEffect, useState } from 'react';
+import { TagListColorPool } from '../../../utils/Constants';
 
-const TagList = ({ data, randomizeColor }) => {
+const TagList = ({ data, randomizeColor, list, className }) => {
+	const [colors, setColors] = useState([]);
+	const [tags, setTags] = useState([]);
 
-	if (randomizeColor) {
-		// TODO
+	const pickVisibleTags = () => {
+		if (list && data && data.length) {
+			let fullString = '';
+			const render = [];
+			for (let i = 0; i < data.length; i++) {
+				fullString += data[i];
+				if (fullString.length > 25) {
+					break;
+				}
+				render.push(data[i]);
+			}
+			setTags(render);
+		} else {
+			setTags(data);
+		}
+	};
+
+	const generateRandomColor = ()=> {
+		if (randomizeColor && tags && tags.length) {
+			let arr = [];
+			for (let i = 0; i < tags.length; i++) {
+				const ind = (Math.floor((Math.random() * 1000) % tags.length));
+				arr.push(TagListColorPool[ind]);
+			}
+			setColors(arr);
+		}
 	}
 
+
+	useEffect(() => {
+		pickVisibleTags();
+		generateRandomColor();
+	}, [data, randomizeColor]);
+
+
 	return (
-		<div className={'tag-list'}>
-			{data && data.map((value, index) => (
+		<div className={!list ? 'tag-grid' : 'tag-list'}>
+			{tags && tags.map((value, index) => (
 				<Tag key={index}
 						 style={{
 							 marginRight: '10px'
-						 }} text={value} />
+						 }} className={(colors[index] || null) + ' ' + className} text={value} />
 			))}
 		</div>
 	);
