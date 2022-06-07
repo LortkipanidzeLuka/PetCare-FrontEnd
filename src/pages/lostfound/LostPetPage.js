be711af06bc4c496e3a1009cbc56ecca9df049ce
tato <tkats18@freeuni.edu.ge>
2022-06-08 01:30:33 +0400

    hide button

--- src/pages/lostfound/LostPetPage.js
+++ src/pages/lostfound/LostPetPage.js
@@ -5,23 +5,27 @@ import { PAGES } from '../../utils/PageConfig';
 import useFetchTrigger from '../../hooks/UseFetchTrigger';
 import Text from '../../components/styled/text/Text';
 import { TextType } from '../../components/styled/text/TextType';
+import { useSelector } from 'react-redux';
+import { userSelectors } from '../../storage/user/Selector';
 
 const LostPetPage = () => {
 	const [, modalOpen, , toggleModal] = useModal();
 	const { Grid, pageConfig } = PAGES.LOST_FOUND;
 	const [updateData, fetchData] = useFetchTrigger();
+	const isLoggedIn = useSelector(userSelectors.isLoggedIn);
+	console.log(isLoggedIn);
 
 	return (
 		<div>
 			<div className={'d-flex justify-content-between'}>
-				<Text text={'Lost And Found Pets'} type={TextType.LARGE}/>
-				<Button onClick={toggleModal} className={'add-button'}>
+				<Text text={'Lost And Found Pets'} type={TextType.LARGE} />
+				{isLoggedIn ? <Button onClick={toggleModal} className={'add-button'}>
 					<Text text={'Add Advertisement'} type={TextType.MEDIUM} />
-				</Button>
+				</Button> : null}
 			</div>
 			<Grid {...pageConfig} updateData={updateData} />
-			<AddLostPet open={modalOpen} closeModal={toggleModal} fetchSingle={pageConfig.fetchSingle}
-									fetchData={fetchData} />
+			{isLoggedIn ? <AddLostPet open={modalOpen} closeModal={toggleModal} fetchSingle={pageConfig.fetchSingle}
+																fetchData={fetchData} /> : null}
 		</div>
 	);
 };
